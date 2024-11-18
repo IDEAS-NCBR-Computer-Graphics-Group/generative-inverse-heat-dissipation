bbe95feaff039a482e3a5196163ad7fab96c4ff2
mwlodarzc <mwlodarzc@gmail.com>
2024-11-18 13:50:44 +0100

    fix: configs, changes required for PR #34

--- configs/afhq/default_lbm_ade_afhq_config.py
+++ configs/afhq/default_lbm_ade_afhq_config.py
@@ -5,26 +5,24 @@ from configs import conf_utils
 from torchvision import transforms
 import os
 
-# This is the 256x256 AFHQ model
-
-
 def get_config():
     return get_default_configs()
 
-
 def get_default_configs():
     config = ml_collections.ConfigDict()
+
     # training
     config.training = training = ml_collections.ConfigDict()
-    config.training.batch_size = 32
-    training.n_evals = 25
-    training.n_iters = 25001 #1300001
-    training.log_freq = 100 # 50
-    training.eval_freq = 200 # 100
-    training.sampling_freq = 2000 #10000
+    training.batch_size = 4
+    training.n_evals = 25 # batches for test-set evaluation, arbitrary choice
+    training.n_iters = 1300001
+    training.log_freq = 1000
+    training.eval_freq = 2000
+    training.sampling_freq = 10000 #10000
+    
     # store additional checkpoints for preemption in cloud computing environments
-    training.snapshot_freq = 5000 #50000
-    training.snapshot_freq_for_preemption = 5000
+    training.snapshot_freq = 50000 # 50000
+    training.snapshot_freq_for_preemption = 50000
 
     training.hash = conf_utils.hash_int(config.training.batch_size)
 
@@ -46,62 +44,66 @@ def get_default_configs():
     data.uniform_dequantization = False
     data.num_channels = 3
 
+    # data - cd
+    data = config.data
     data.showcase_comparison = True
     data.process_all = True
     data.process_pairs = True
-    data.processed_filename = 'lbm_ns_pairs' if data.process_pairs else 'lbm_ns'
+    data.processed_filename = 'lbm_ade_pairs' if data.process_pairs else 'lbm_ade'
     data.dataset = 'AFHQ'
-    data.image_size = 256
-    data.transform = transforms.Compose([
-        transforms.ToTensor()
-        ])
+    data.image_size = 128
+    data.transform = transforms.Compose(
+        [transforms.ToTensor()])
+
 
     # solver
-    config.turbulence = turbulence = ml_collections.ConfigDict()
-    turbulence.turb_intensity = float(0.) #*1E-4
-    turbulence.noise_limiter = (-1E-3, 1E-3)
+    solver = config.solver =  ml_collections.ConfigDict()
+    solver.num_channels = data.num_channels
+    solver.type = 'ade'
+    solver.min_init_gray_scale = 0.95
+    solver.max_init_gray_scale = 1.05
+    solver.min_fwd_steps = 1
+    solver.n_denoising_steps = 200
+    solver.max_fwd_steps = solver.n_denoising_steps + 1  # corruption_amount = np.random.randint(self.min_steps, self.max_steps) thus we need to add +1 as max_fwd_steps is excluded from tossing
+    solver.final_lbm_step = 500
+
+    # solver.corrupt_sched = conf_utils.lin_schedul(
+    #         solver.min_fwd_steps, solver.final_lbm_step, solver.max_fwd_steps, dtype=int)
+    
+    solver.corrupt_sched = conf_utils.exp_schedule(
+        solver.min_fwd_steps, solver.final_lbm_step, solver.max_fwd_steps, dtype=int)
+        
+    are_steps_unique = True
+    if are_steps_unique:
+        solver.corrupt_sched = np.unique(solver.corrupt_sched)
+        solver.max_fwd_steps = len(solver.corrupt_sched)
+        solver.n_denoising_steps = solver.max_fwd_steps - 1
+    
+    solver.cs2 = conf_utils.lin_schedule(1./3, 1./3, solver.final_lbm_step, dtype=np.float32)
+    # niu_sched = conf_utils.lin_schedule(1E-4*1 / 6, 1E-4 * 1 / 6, solver.final_lbm_step, dtype=np.float32)
+    niu_sched = conf_utils.tanh_schedule(1E-3* 1./ 6,  1./ 6, solver.final_lbm_step, dtype=np.float32)
+    # niu_sched  = conf_utils.exp_schedule(1E-4 * 1./6., 1./6., solver.max_fwd_steps)
+
+    solver.niu = solver.bulk_visc = niu_sched
+    solver.hash = conf_utils.hash_solver(solver)
+
+    # turbulence
+    turbulence = config.turbulence = ml_collections.ConfigDict()
+    turbulence.turb_intensity = conf_utils.lin_schedule(1E-6, 1E-2, solver.final_lbm_step, dtype=np.float32)
+    turbulence.noise_limiter = (-1E-2, 1E-2)
     turbulence.domain_size = (1.0, 1.0)
-    turbulence.dt_turb = 5 * 1E-4
+    turbulence.dt_turb = 1 * 1E-3
     turbulence.k_min = 2.0 * torch.pi / min(turbulence.domain_size)
     turbulence.k_max = 2.0 * torch.pi / (min(turbulence.domain_size) / 1024)
     turbulence.is_divergence_free = False
     turbulence.energy_slope = -5.0 / 3.0
     turbulence.hash = conf_utils.hash_solver(turbulence)
     turbulence.energy_spectrum = lambda k: torch.where(torch.isinf(k ** (turbulence.energy_slope)), 0, k ** (turbulence.energy_slope))
-    
-    config.solver = solver = ml_collections.ConfigDict()
-    solver.type = 'ns'
-    solver.min_init_gray_scale = 0.95
-    solver.max_init_gray_scale = 1.05
-    solver.cs2 = 1./3.
-    solver.min_fwd_steps = 1
-    solver.n_denoising_steps = 50
-    solver.max_fwd_steps = solver.n_denoising_steps + 1 # corruption_amount = np.random.randint(self.min_steps, self.max_steps) thus we need to add +1 as max_fwd_steps is excluded from tossing
-    solver.final_lbm_step = 50
-    solver.lin_sched = False
-
-    if solver.lin_sched: 
-        solver.corrupt_sched = np.linspace(
-            solver.min_fwd_steps, solver.final_lbm_step, solver.max_fwd_steps, dtype=int)
-    else:
-        solver.lbm_steps_base = 2.0
-        solver.starting_lbm_steps_pow = np.emath.logn(solver.lbm_steps_base, solver.min_fwd_steps)
-        solver.final_lbm_steps_pow = np.emath.logn(solver.lbm_steps_base, solver.final_lbm_step)
-        if np.math.pow(solver.lbm_steps_base, solver.final_lbm_steps_pow) != solver.final_lbm_step:
-            solver.final_lbm_steps_pow += 2*np.finfo(float).eps
-        solver.corrupt_sched = np.logspace(
-            solver.starting_lbm_steps_pow, solver.final_lbm_steps_pow,
-            solver.max_fwd_steps, base=solver.lbm_steps_base, dtype=int)
-
-    niu_sched = conf_utils.lin_schedule(1E-4 * 1 / 6, 1 / 6, solver.max_fwd_steps)
-    solver.niu = solver.bulk_visc = niu_sched
-    solver.hash = conf_utils.hash_solver(solver)
 
     # model
     config.model = model = ml_collections.ConfigDict()
+    
     model.sigma = 0.01
-    model.beta_min = 0.1
-    model.beta_max = 20.
     model.dropout = 0.3
     model.model_channels = 128
     model.channel_mult = (1, 2, 3, 4, 5)
@@ -126,12 +128,12 @@ def get_default_configs():
     config.optim = optim = ml_collections.ConfigDict()
     optim.weight_decay = 0
     optim.optimizer = 'Adam'
-    optim.lr = 1e-4
+    optim.lr = 2e-5
     optim.beta1 = 0.9
     optim.eps = 1e-8
     optim.warmup = 5000
     optim.grad_clip = 1.
-    optim.automatic_mp = False  # Automatic mixed precision
+    optim.automatic_mp = True
     optim.hash = conf_utils.hash_solver(optim)
 
     config.stamp = stamp = ml_collections.ConfigDict()
@@ -143,7 +145,7 @@ def get_default_configs():
     config.device = torch.device(
         'cuda:0') if torch.cuda.is_available() else torch.device('cpu')
 
-    if os.uname().nodename in ['pc']:
+    if os.uname().nodename in ['pc', 'laptop']:
         debug = True
     else:
         debug = False
@@ -162,6 +164,7 @@ def get_default_configs():
         config.training.batch_size = 4 # rtx2080
         config.eval.batch_size = 4
         config.training.n_iters = 1001
+        config.training.eval_freq = 100
         config.training.sampling_freq = 100
         
     return config
