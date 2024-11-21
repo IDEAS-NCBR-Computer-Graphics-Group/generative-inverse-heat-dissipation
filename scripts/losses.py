af54fe77711c8eb2ddd7d52c133d1149f021f157
Jakub Meixner <zakazm@op.pl>
2024-11-21 15:38:54 +0100

    gradscaler

--- scripts/losses.py
+++ scripts/losses.py
@@ -127,9 +127,9 @@ def get_step_lbm_fn(train, config,
     loss_fn = get_inverse_lbm_ns_loss_fn(train, config.model.sigma)
     
     # For automatic mixed precision
-    scaler = torch.cuda.amp.GradScaler()
+    # scaler = torch.cuda.amp.GradScaler()
     # scaler = torch.cuda.amp.GradScaler('cuda')
-    # scaler = torch.amp.GradScaler("cuda")
+    scaler = torch.amp.GradScaler("cuda")
     def step_fn(state, batch):
         """Running one step of training or evaluation.
         Returns:
