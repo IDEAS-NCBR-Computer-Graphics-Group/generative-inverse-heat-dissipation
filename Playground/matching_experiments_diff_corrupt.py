74a308d0ebc1d825c065a2548c7d8d1769908877
mwlodarzc <mwlodarzc@gmail.com>
2024-11-28 16:28:56 +0100

    feat: matching found for difference of sigmas

--- Playground/matching_experiments_diff_corrupt.py
+++ Playground/matching_experiments_diff_corrupt.py
@@ -1,29 +1,13 @@
 #%% import stufff
 import numpy as np
-from numba import jit, njit
-from numpy.testing import assert_almost_equal
-from numpy.fft import fft2, fftshift, ifft2
-import matplotlib
 import matplotlib.pyplot as plt
-import time
 import torch
-from scipy.ndimage import gaussian_filter
-from torchvision.transforms import GaussianBlur
-import torch.nn as nn
 import torch
-import torch.nn as nn
-import torch.optim as optim
-import math
 import taichi as ti
-import taichi.math as tm
 from model_code.utils import DCTBlur
 from numerical_solvers.solvers.LBM_ADE_Solver import LBM_ADE_Solver
-from numerical_solvers.corruptors.LBM_ADE_Corruptor import LBM_ADE_Corruptor
-
 from tests.configs.ffhq_128_lbm_ade_example import get_config
-# from configs.mnist.small_mnist_lbm_ade_turb_config import get_config
 from numerical_solvers.solvers.SpectralTurbulenceGenerator import SpectralTurbulenceGenerator
-from numerical_solvers.visualization.CanvasPlotter import CanvasPlotter
 from configs import conf_utils, match_sim_numbers
 from numerical_solvers.solvers.img_reader import read_img_in_grayscale, normalize_grayscale_image_range
 
@@ -38,12 +22,8 @@ def load_image(config, L):
 def display_schedules(Fo, Fo_realizable):
     fig = plt.figure(figsize=(8, 8))
     ax = fig.add_axes([0.2, 0.2, 0.7, 0.7])
-    # plt.plot(niu_array,  'rx', label=f'niu_array')
     plt.plot(Fo,  'rx', label=f'Fo_schedule')
     plt.plot(Fo_realizable,  'gx', label=f'Fo_realizable')
-    # plt.plot(corrupt_sched, Fo_realizable, 'bx', label=f'Fo_realizable(lbm_steps)')
-    # plt.plot(np.unique(Fo_schedule_unique), 'bx', label=f'Fo_schedule_unique')
-
 
     ax.grid(True, which="both", ls="--")
     ax.set_xlabel(r"denoising steps")
@@ -56,20 +36,26 @@ def map01(image):
     max_val = image.max()
     return (image - min_val) / (max_val - min_val)
 
+def calc_diff(x, y):
+    return np.sqrt(x**2 - y**2)
+
 def main():
     config = get_config()
-    model = config.model
 
-    model.K = 20
-    model.blur_sigma_max = 16
-    model.blur_sigma_min = 0.5
-    model.blur_schedule = np.exp(np.linspace(np.log(model.blur_sigma_min), np.log(model.blur_sigma_max), model.K))
-    model.blur_schedule = np.array([0] + list(model.blur_schedule))
+    config.model.K = 20
+    config.model.blur_sigma_max = 16
+    config.model.blur_sigma_min = 0.5
+    config.model.blur_schedule = np.exp(np.linspace(
+        np.log(config.model.blur_sigma_min),
+        np.log(config.model.blur_sigma_max),
+        config.model.K
+        ))
+    config.model.blur_schedule = np.array([0] + list(config.model.blur_schedule))
 
-    bs = model.blur_schedule
-    bs_diff = [np.sqrt(bs[i]**2 - bs[i-1]**2) for  i in range(1,len(bs))]
+    bs = config.model.blur_schedule
+    bs_diff = [calc_diff(bs[i], bs[i-1]) for  i in range(1,len(bs))]
     bs_diff = [bs[0] , *bs_diff]
-    dctBlur = DCTBlur(model.blur_schedule, image_size=config.data.image_size, device="cpu")
+    dctBlur = DCTBlur(config.model.blur_schedule, image_size=config.data.image_size, device="cpu")
     dctBlur_diff = DCTBlur(bs_diff, image_size=config.data.image_size, device="cpu")
 
     print(bs)
@@ -79,55 +65,46 @@ def main():
     np_gray_image = load_image(config, L)
     # np_gray_image_rot90 = np.rot90(np_gray_image.copy(), k=-1)
 
-    match_sim_numbers.plot_matrix(np_gray_image, title="IC")
-    t_initial_condition = torch.from_numpy(np_gray_image).unsqueeze(0)
+    match_sim_numbers.plot_matrix(map01(np_gray_image), title="IC")
     t_init = torch.from_numpy(np_gray_image).unsqueeze(0)
 
-    fwd_steps = model.K * torch.ones(1, dtype=torch.long)
-    blurred_by_dct = dctBlur(t_initial_condition, fwd_steps).float().squeeze().numpy()
-    match_sim_numbers.plot_matrix(blurred_by_dct, title=f'DCT schedule Blurr for sigma')
+    fwd_steps = config.model.K * torch.ones(1, dtype=torch.long)
+    blurred_by_dct = dctBlur(t_init, fwd_steps).float().squeeze().numpy()
+    match_sim_numbers.plot_matrix(map01(blurred_by_dct), title=f'DCT schedule Blurr for sigma')
 
-    # blurred_by_dct_m1 = np_gray_image.copy()
-    # for i in range(len(model.blur_schedule)):
-    #     fwd_steps = i * torch.ones(1, dtype=torch.long)
+    blurred_by_dct_all = []
+    blurred_by_delta_dct_all = []
+    blurred_by_dct_m1 = np_gray_image.copy()
 
-    #     print('NEXT')
-    #     print('dct')
-    #     blurred_by_dct = dctBlur(t_initial_condition, fwd_steps).float().squeeze().numpy()
-    #     delta_blurr = blurred_by_dct - np_gray_image
-    #     match_sim_numbers.plot_matrix(blurred_by_dct, title=f'DCT schedule Blurr for sigma = {model.blur_schedule[i]:3.4e}')
-    #     match_sim_numbers.plot_matrix(delta_blurr, title=f'delta_blurr for delta sigma = {model.blur_schedule[i]:3.4e}')
+    for i in range(len(config.model.blur_schedule)):
+        fwd_steps = i * torch.ones(1, dtype=torch.long)
+        blurred_by_dct = dctBlur(t_init, fwd_steps).float().squeeze().numpy()
+        blurred_by_dct_all.append(map01(blurred_by_dct))
 
-    #     print('dct_diff')
-    #     blurred_by_dct_diff = dctBlur_diff(torch.Tensor(blurred_by_dct_m1.copy()).unsqueeze(0), fwd_steps).float().squeeze().numpy()
-    #     delta_blurr_diff = blurred_by_dct_diff - blurred_by_dct
-    #     match_sim_numbers.plot_matrix(blurred_by_dct_diff, title=f'DCT schedule Blurr for delta_sigma = {bs_diff[i]:3.4e}')
-    #     match_sim_numbers.plot_matrix(delta_blurr_diff, title=f'delta_blurr for delta sigma = {bs_diff[i]:3.4e}')
-    #     # match_sim_numbers.plot_matrix(blu, title=f'delta_blurr for delta sigma = {bs_diff[i]:3.4e}')
+        blurred_by_dct_diff = dctBlur_diff(torch.Tensor(blurred_by_dct_m1.copy()).unsqueeze(0), fwd_steps).float().squeeze().numpy()
+        blurred_by_delta_dct_all.append(map01(blurred_by_dct_diff))
+        blurred_by_dct_m1 = blurred_by_dct_diff
 
-    #     blurred_by_dct_m1 = blurred_by_dct_diff
+    match_sim_numbers.plot_matrices_in_grid(blurred_by_dct_all, columns=4)
+    match_sim_numbers.plot_matrices_in_grid(blurred_by_delta_dct_all, columns=4)
 
     dFo_array = match_sim_numbers.calc_Fo(bs_diff, L)
 
     niu_min = 1E-4 * 1/6
     niu_max = 1./6
 
-    (dt_array, 
-     niu_array, 
-     niu_array_realizable, 
-     dFo_realizable) = match_sim_numbers.calculate_t_niu_array(dFo_array, niu_min, niu_max, L)
+    dt_array, niu_array, *_ = match_sim_numbers.calculate_t_niu_array_from_0(
+        dFo_array, niu_min, niu_max, L)
 
-    Fo_realizable =  np.array(np.cumsum(dFo_realizable))
-    corrupt_sched =  np.array(list(np.cumsum(dt_array)), dtype=int) # scan add
+    corrupt_sched =  np.array(list(np.cumsum(dt_array)), dtype=int)
     print(f'niu_array = {niu_array}')
-    print(f'niu_array_realizable = {niu_array_realizable}')
-    print(f'sigma = {model.blur_schedule}')
+    print(f'sigma = {config.model.blur_schedule}')
     print(f'Fo={dFo_array}')
     print(f'dt_array = {dt_array}')
     print(f'corrupt_sched = {corrupt_sched}')
 
-    config.solver.max_fwd_steps = model.K
-    config.solver.n_denoising_steps = model.K - 1
+    config.solver.max_fwd_steps = config.model.K
+    config.solver.n_denoising_steps = config.model.K - 1
     config.solver.corrupt_sched = corrupt_sched
 
     config.solver.cs2 = conf_utils.lin_schedule(1./3, 1./3, corrupt_sched[-1], dtype=np.float32)
@@ -135,48 +112,40 @@ def main():
     config.solver.niu = config.solver_bulk_visc = np.array(sum([[niu_array[i]]*dt_array[i] for i in range(len(niu_array))], []))
     config.solver.final_lbm_step = int(corrupt_sched[-1])
 
-    # # spectralTurbulenceGenerator = SpectralTurbulenceGenerator(
-    # #     config.turbulence.domain_size,
-    # #     (L, L), 
-    # #     0 * config.turbulence.turb_intensity,
-    # #     config.turbulence.noise_limiter,
-    # #     energy_spectrum=config.turbulence.energy_spectrum, 
-    # #     frequency_range={'k_min': config.turbulence.k_min, 'k_max': config.turbulence.k_max}, 
-    # #     dt_turb=config.turbulence.dt_turb, 
-    # #     is_div_free=False
-    # #     )
+    np_gray_image_rot90 = np.rot90(np_gray_image.copy(), k=-1)
 
-    # # p_niu = 1e-3
-    # # p_corrupt = 200
+    spectralTurbulenceGenerator = SpectralTurbulenceGenerator(
+        config.turbulence.domain_size,
+        (L, L), 
+        0 * config.turbulence.turb_intensity,
+        config.turbulence.noise_limiter,
+        energy_spectrum=config.turbulence.energy_spectrum, 
+        frequency_range={'k_min': config.turbulence.k_min, 'k_max': config.turbulence.k_max}, 
+        dt_turb=config.turbulence.dt_turb, 
+        is_div_free=False
+        )
 
-    # # p_niu_range = (1e-16, 1)
-    # # p_corrupt = (model.K, 1000)
+    print(config.solver.niu)
+    solver = LBM_ADE_Solver(
+        (L, L),
+        config.solver.niu,
+        config.solver.niu,
+        conf_utils.lin_schedule(1./3, 1./3, config.solver.final_lbm_step, dtype=np.float32),
+        spectralTurbulenceGenerator
+        )
 
-    # niu_array = conf_utils.tanh_schedule(p_niu* 1./ 6,  1./ 6, config.solver.final_lbm_step, dtype=np.float32)
-    # # niu_sched  = conf_utils.exp_schedule(1E-4 * 1./6., 1./6., solver.max_fwd_steps)
-    # config.corrupt_sched = conf_utils.exp_schedule(1, p_corrupt, config.solver.max_fwd_steps, dtype=int)
-        
-    corruptor = LBM_ADE_Corruptor(config=config, transform=config.data.transform)
-    _ = corruptor._corrupt(t_init, config.solver.n_denoising_steps)
-    
-    # solver.solve(iterations=sum(dt_array))
-    img = corruptor.intermediate_samples[-1]
-    blurred_by_lbm = np.rot90(img.numpy().copy(), k=0).squeeze(0) # torch to numpy + rotation
-    blurred_by_lbm_all = [map01(i.squeeze(0)) for i in corruptor.intermediate_samples]
-    # print(blurred_by_dct.shape)
-    # print(blurred_by_lbm.shape)
-    # # blurred_by_lbm = np.transpose(blurred_by_lbm, (1, 0, 2))
-    # print(blurred_by_lbm.shape)
-    # blurred_by_lbm = blurred_by_lbm.squeeze(0)
+    solver.init(np_gray_image_rot90.copy()) 
+    lbm_corrupted = [map01(np_gray_image)]
+    for lbm_iter in dt_array:
+        solver.solve(iterations=lbm_iter)
+        img = solver.rho
+        blurred_by_lbm = np.rot90(img.to_numpy(), k=1)
+        lbm_corrupted.append(map01(blurred_by_lbm))
 
-
-    match_sim_numbers.plot_matrices_in_grid(blurred_by_lbm_all, columns=4)
-    match_sim_numbers.plot_matrix_side_by_side(blurred_by_lbm, blurred_by_dct, title="LBM BLURR")
-    match_sim_numbers.plot_matrix(blurred_by_dct-blurred_by_lbm, title="Difference DCT schedule - LBM")
-    print(f'Difference: {abs(blurred_by_dct - blurred_by_lbm)}')
-    # print(f'Maximal value of DCT blurr: {blurred_by_dct.max()}')
-    # print(f'Maximal value of LBM blurr: {blurred_by_lbm.max()}')
-
+    match_sim_numbers.plot_matrices_in_grid(lbm_corrupted, columns=4)
+    match_sim_numbers.plot_matrix_side_by_side(lbm_corrupted[-1], blurred_by_dct_all[-1], title="LBM BLURR")
+    match_sim_numbers.plot_matrix(blurred_by_dct_all[-1]-lbm_corrupted[-1], title="Difference DCT schedule - LBM", range=(0,1))
+    print(f'Difference: {np.mean((blurred_by_dct_all[-1]-lbm_corrupted[-1])**2)}')
 
 if __name__ == '__main__':
     ti.init(arch=ti.gpu) if torch.cuda.is_available() else ti.init(arch=ti.cpu)
