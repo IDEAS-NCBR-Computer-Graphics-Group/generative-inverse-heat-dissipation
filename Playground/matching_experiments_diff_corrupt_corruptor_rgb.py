fix: repair overflow values, error borders with mnist training

Playground/matching_experiments_diff_corrupt_corruptor_rgb.py
@@ -17,16 +17,14 @@ from numerical_solvers.visualization import plotting
 def load_image(config, L):
     img_path = './numerical_solvers/runners/cat_768x768.jpg'
     image = cv2.imread(img_path) 
-    image = cv2.resize(image, (L,L))
+    image = cv2.resize(image, (L, L))
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image = config.data.transform(image)
     image = np.array(image)
     return image
 
 def map01(image):
-    min_val = image.min()
-    max_val = image.max()
-    return (image - min_val) / (max_val - min_val)
+    return image
 
 def calc_diff(x, y):
     return np.sqrt(x**2 - y**2)
@@ -103,7 +101,7 @@ def main():
 
     corruptor = LBM_ADE_Corruptor(config=config, transform=config.data.transform)
     _ = corruptor._corrupt(t_init.squeeze(0), config.solver.max_fwd_steps)
-    lbm_corrupted = [np.transpose(i.squeeze(0).numpy(), (1,2,0)) for i in corruptor.intermediate_samples]
+    lbm_corrupted = [(np.transpose(i.squeeze(0).numpy(), (1,2,0))) for i in corruptor.intermediate_samples]
 
     plotting.plot_matrices_in_grid(lbm_corrupted, columns=4)
     plotting.plot_matrix_side_by_side(lbm_corrupted[-1], blurred_by_dct_all[-1], title="LBM BLURR")

numerical_solvers/solvers/img_reader.py
@@ -66,4 +66,8 @@ def normalize_grayscale_image_range(image, min_val, max_val) -> np.array:
 
 def change_value_range(image, prev_min_val, prev_max_val, min_val, max_val) -> np.array:
     image = image.astype(np.float32)
-    return min_val + (image - prev_min_val) * (max_val - min_val) / (prev_max_val - prev_min_val)
+    epsilon = 1e-8
+    denominator = (prev_max_val - prev_min_val) + epsilon
+    scaled = min_val + (image - prev_min_val) * (max_val - min_val) / denominator
+    scaled = np.clip(scaled, min_val, max_val)
+    return scaled

numerical_solvers/visualization/plotting.py
@@ -25,11 +25,12 @@ def plot_matrices_in_grid(matrices, titles=None, columns=5, value_range=None):
     cols = columns
     rows = int(np.ceil(num_matrices / cols))
 
-    if value_range is None:
-        vmin = min(matrix.min() for matrix in matrices)
-        vmax = max(matrix.max() for matrix in matrices)
-    else:
-        vmin, vmax = value_range
+    # if value_range is None:
+    #     vmin = min(matrix.min() for matrix in matrices)
+    #     vmax = max(matrix.max() for matrix in matrices)
+    # else:
+    #     vmin, vmax = value_range
+    vmin, vmax = None, None
 
     fig, axes = plt.subplots(rows, cols, figsize=(cols * 5, rows * 4))
     axes = np.array(axes).reshape(-1)