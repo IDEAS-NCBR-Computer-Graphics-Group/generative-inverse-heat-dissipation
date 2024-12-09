8259b72cfd5219d57d3751703435d64535dc8f7b
mwlodarzc <mwlodarzc@gmail.com>
2024-12-09 16:01:01 +0100

    fix+refactor: update configs for ihd matching, clean-up all dataset configs

--- configs/mnist/ihd/small_mnist.py
+++ configs/mnist/ihd/small_mnist.py
@@ -5,7 +5,6 @@ import numpy as np
 def get_config():
     config = default_mnist_configs.get_default_configs()
     model = config.model
-    
     model.blur_sigma_max = 20
     model.blur_sigma_min = 0.5
     model.model_channels = 64
@@ -13,11 +12,8 @@ def get_config():
     model.K = 50
     model.blur_schedule = np.exp(np.linspace(np.log(model.blur_sigma_min),
                                              np.log(model.blur_sigma_max), model.K))
-    model.blur_schedule = np.array([0] + list(model.blur_schedule))  # Add the k=0 timestep
-    
-    
-    config.training.n_iters = 10001
-    config.training.snapshot_freq = 1000
-    config.training.snapshot_freq_for_preemption = 500
-    config.training.sampling_freq = 500
+    model.blur_schedule = np.array(
+        [0] + list(model.blur_schedule))  # Add the k=0 timestep
+    config.training.snapshot_freq_for_preemption = 100
+    config.training.sampling_freq = 1000
     return config
